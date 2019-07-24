d29597718a232a8e6da384bad7da6f58cb013bd0
letscook1 <mmwilliamscodes@gmail.com>
2019-07-24 14:30:58 -0400

    more changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,13 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
-  id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
+CREATE TABLE burgers
+(
+  id INT
+  AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR
+  (100) NOT NULL,
   devoured BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY (id)
+  PRIMARY KEY
+  (id)
 );
